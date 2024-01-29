64cc6dfdf9dbdd5c2f50d82acfa4d949bfed8069
Mariola <mariolahullings@gmail.com>
2024-01-29 15:15:09 -0500

    Initial Commit

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Mariola Hullings on 1/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
